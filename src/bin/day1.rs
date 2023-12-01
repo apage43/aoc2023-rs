910e5297e96434b941c63cb1bd02091615d5a0a8
aaron miller <apage43@ninjawhale.com>
2023-12-01 00:47:33 -0800

    d1p1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -3,5 +3,18 @@ use std::io;
 fn main() -> Result<()> {
     color_eyre::install()?;
     let input = io::read_to_string(io::stdin())?;
+    let total: u32 = input.lines().filter_map(|line| {
+        let mut it = line
+            .chars()
+            .filter(char::is_ascii_digit)
+            .filter_map(|c| c.to_digit(10));
+        let first = it.next();
+        let last = match it.last() {
+            None => first,
+            last => last,
+        };
+        Some(first? * 10 + last?)
+    }).sum();
+    println!("total: {total}");
     Ok(())
 }
