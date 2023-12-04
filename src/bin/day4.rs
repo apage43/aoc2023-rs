fca484ddb5cb4abd7d54175d96e098c4fb5cc8e7
aaron miller <apage43@ninjawhale.com>
2023-12-03 21:54:54 -0800

    day4p2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use color_eyre::Result;
 use scan_fmt::scan_fmt;
 
@@ -10,10 +12,13 @@ struct Card {
 
 impl Card {
     fn num_winners(&self) -> usize {
-        self.have.iter().filter_map(|h| self.winners.contains(h).then_some(1)).sum()
+        self.have
+            .iter()
+            .filter_map(|h| self.winners.contains(h).then_some(1))
+            .sum()
     }
     fn points(&self) -> usize {
-       (0..self.num_winners()).fold(0, |v, _| if v == 0  {1 } else { v * 2})
+        (0..self.num_winners()).fold(0, |v, _| if v == 0 { 1 } else { v * 2 })
     }
 }
 fn main() -> Result<()> {
@@ -38,10 +43,37 @@ fn main() -> Result<()> {
         })
         .collect::<Vec<_>>();
     for card in &cards {
-        eprintln!("card {}, {} winners, {} points", card.id, card.num_winners(), card.points());
+        eprintln!(
+            "card {}, {} winners, {} points",
+            card.id,
+            card.num_winners(),
+            card.points()
+        );
     }
-    //dbg!(&cards);
     let points: usize = cards.iter().map(Card::points).sum();
     dbg!(points);
+
+    let cards_by_id: HashMap<usize, Card> =
+        HashMap::from_iter(cards.into_iter().map(|c| (c.id, c)));
+    let mut queued_cards: Vec<usize> = cards_by_id.keys().copied().collect();
+    let mut processed_cards: HashMap<usize, usize> = Default::default();
+    loop {
+        let mut won_cards = vec![];
+        for card in queued_cards.iter() {
+            let won = cards_by_id[card].num_winners();
+            won_cards.extend(
+                ((card + 1)..)
+                    .take(won)
+                    .take_while(|c| cards_by_id.contains_key(c)),
+            );
+            *processed_cards.entry(*card).or_default() += 1;
+        }
+        if won_cards.is_empty() { break }
+        queued_cards.clear();
+        queued_cards.append(&mut won_cards);
+    }
+    dbg!(&processed_cards);
+    dbg!(processed_cards.values().sum::<usize>());
+
     Ok(())
 }
